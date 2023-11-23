c4301cc07148333ef9ef37e271e712b67f0f7aa9
Ariful2634 <ariful2634@gmail.com>
2023-11-23 19:51:50 +0600

    set up the navbar on the ui

---
+++ src/Components/Pages/Shared/Navbar.jsx
@@ -0,0 +1,67 @@
+import {  NavLink } from "react-router-dom";
+import tech from '../Shared../../../Pages../../../assets/tech-hub-removebg-preview.png'
+
+
+const Navbar = () => {
+
+    const links = <>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/products'>Products</NavLink></li>
+    </>
+
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {links}
+                        </ul>
+                    </div>
+                    <img className="w-[150px]" src={tech} alt="" />
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {links}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                {/* <div>
+                        {
+                            user?.email ? <div className="dropdown dropdown-end">
+                                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                    <div className="w-10 rounded-full">
+                                        {
+                                            user && <img src={user.photoURL} />
+                                        }
+                                    </div>
+                                </label>
+                                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                    <li>
+                                        <button className="btn btn-sm  btn-ghost">{
+                                            user && <p>{user.displayName}</p>
+                                        }</button>
+
+                                    </li>
+                                    <li>
+                                        <button onClick={handleOut} className="btn btn-sm  btn-ghost">Logout</button>
+
+                                    </li>
+                                </ul>
+                            </div>
+                                :
+                                <Link to='/login'>
+                                    <button className="btn btn-sm mr-2  btn-outline btn-warning">Login</button>
+                                </Link>
+                        }
+                    </div> */}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
