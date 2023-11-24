42f5f6e20765ebbd16691c5148f3dbd769369b78
Ariful2634 <ariful2634@gmail.com>
2023-11-24 22:25:26 +0600

    added product details page

--- src/Components/Pages/Home/FeatureDetails.jsx
+++ src/Components/Pages/Home/FeatureDetails.jsx
@@ -14,14 +14,16 @@ const FeatureDetails = () => {
 
     return (
         <div>
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card w-[400px] ml-3 bg-base-100 shadow-xl mt-10">
                     <figure><img className="h-[200px]" src={details.product_image} alt="Shoes" /></figure>
                     <div className="card-body">
                         <h2 className="card-title">{details.product_name}</h2>
-                        {/* <p className="space-x-3"><span className="font-bold text-blue-600">Tags:</span> <span>#{tags[0]}</span> <span>#{tags[1]}</span> <span>#{tags[2]}</span></p> */}
-                        <div className="card-actions justify-between">
-                            {/* <p>{timestamp}</p> */}
+                        <p>{details.description}</p>
+                        <p className="space-x-3"><span className="font-bold text-blue-600">Tags:</span> <span>#{details.tags[0]}</span> <span>#{details.tags[1]}</span> <span>#{details.tags[2]}</span></p>
+                        <div className="card-actions justify-between mt-4">
+                            
                             <button className="btn btn-primary"><GiVote></GiVote>Upvote </button>
+                            <button className="btn btn-error">Report</button>
 
                         </div>
                     </div>
