c4379ada7a2d389e63b0a5c1e74a229881c4e11f
Ariful2634 <ariful2634@gmail.com>
2023-11-27 12:32:51 +0600

    moderator can accept products

--- src/Components/Pages/Dashboard/Moderator/ProductReviewQueue.jsx
+++ src/Components/Pages/Dashboard/Moderator/ProductReviewQueue.jsx
@@ -2,13 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
+// import { useState } from "react";
 
 
 const ProductReviewQueue = () => {
 
     const axiosSecure = useAxiosSecure()
+    // const {confirm,setConfirm}=useState(false)
 
-    const { data: products = [] } = useQuery({
+    const {refetch, data: products = [] } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('/addProduct')
@@ -41,6 +43,24 @@ const ProductReviewQueue = () => {
         })
     }
 
+    const handleAccept = (id)=>{
+        const accepted = {status:'accept'}
+        console.log(accepted)
+        axiosSecure.put(`/addProduct/status/${id}`,accepted)
+        .then(res=>{
+            console.log(res.data)
+        })
+        // console.log(id)
+        refetch()
+
+
+
+    }
+
+   
+
+  
+
     return (
         <div>
             <div className="overflow-x-auto flex lg:flex-row flex-col">
@@ -64,9 +84,9 @@ const ProductReviewQueue = () => {
                                 <td>{product.product_name}</td>
                                 <td> <Link to={`/featureDetails/${product._id}`}><button className="btn btn-accent">View Details</button></Link> </td>
                                 <td><button onClick={()=>handleFeatured(product._id)} className="btn btn-accent">Featured</button></td>
-                                <td><button  className="btn btn-accent">Accept</button></td>
+                                <td><button onClick={()=>handleAccept(product._id)} className="btn btn-accent">Accept</button></td>
                                 <td><button className="btn btn-accent">Reject</button></td>
-                                <td>Pending</td>
+                                <td> {product.status === 'accept' ? "Accept" : "Pending"}</td>
 
 
                             </tr>)
