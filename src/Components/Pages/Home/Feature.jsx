262518136b61ba50bfc0d320dd658733bedc9a7a
Ariful2634 <ariful2634@gmail.com>
2023-11-29 00:18:49 +0600

    add sorting on the featured data

--- src/Components/Pages/Home/Feature.jsx
+++ src/Components/Pages/Home/Feature.jsx
@@ -1,73 +1,68 @@
 /* eslint-disable react/prop-types */
-// import { useContext } from "react";
-// import { useState } from "react";
+import { useContext } from "react";
 import { GiVote } from "react-icons/gi";
 import { Link } from "react-router-dom";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
 import Swal from "sweetalert2";
 import { AuthContext } from "../../Provider/AuthProvider";
-import { useContext } from "react";
-// import { AuthContext } from "../../Provider/AuthProvider";
 
-const Feature = ({ product,refetch }) => {
-
-    const {user}=useContext(AuthContext)
-    // const [count,setCount]=useState(0)
-
+const Feature = ({ product, refetch }) => {
+    const { user } = useContext(AuthContext);
     const mail = user?.email === product?.email;
-    console.log(mail)
+    const { _id, product_image, product_name, tags, timestamp, upvoted, voteEmail } = product;
+    const voteMail = user?.email === voteEmail;
+    const axiosPublic = useAxiosPublic();
 
-    const { _id, product_image, product_name, tags, timestamp, upvoted,voteEmail } = product
-
-    const voteMail = user?.email===voteEmail
-
-    const axiosPublic = useAxiosPublic()
-
-
-    const handleUpvote = id=>{
-        // let upvoted = 0
-        const upvote = { upvote: upvoted + 1, voteEmail:user?.email }
-        console.log(upvote)
-        axiosPublic.put(`/addProduct/upvote/${id}`, upvote)
-            .then(res => {
-                console.log(res.data)
-                if(res.data.modifiedCount > 0){
-                    refetch()
-                    Swal.fire({
-                        position: "top-end",
-                        icon: "success",
-                        title: "You vote this successfully",
-                        showConfirmButton: false,
-                        timer: 1500
-                    });
-                }
-                
-            })
-        console.log(id)
-    }
-
-   
-
+    const handleUpvote = (id) => {
+        const upvote = { upvote: upvoted + 1, voteEmail: user?.email };
+        axiosPublic.put(`/addProduct/upvote/${id}`, upvote).then((res) => {
+            if (res.data.modifiedCount > 0) {
+                refetch();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "You voted successfully",
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+            }
+        });
+    };
 
     return (
         <div>
-            
-                <div className="card w-96  p-4 bg-base-100 shadow-xl">
-                    <figure><img className="h-[200px]" src={product_image} alt="Shoes" /></figure>
-                    <div className="card-body">
-                    <Link to={`/featureDetails/${_id}`}> <h2 className="card-title">Name: {product_name}</h2></Link>
-                        <p className="space-x-3"><span className="font-bold text-blue-600">Tags:</span> <span>#{tags}</span> </p>
-                        <div className="card-actions justify-between items-center mt-4">
-                            <p>{timestamp}</p>
-                           {
-                           voteMail ||  mail || !user?  <Link to='/login'><button  className="btn text-blue-600 w-full  mt-4 bg-gradient-to-r from-cyan-600 to-pink-300 " disabled><GiVote></GiVote>Upvote ({upvoted})</button></Link> :
-                            <button onClick={()=>handleUpvote(_id)} className="btn text-white w-full mt-4 bg-gradient-to-r from-cyan-600 to-pink-300"><GiVote></GiVote>Upvote ({upvoted})</button>
-                           }
-
-                        </div>
+            <div className="card w-96 p-4 bg-base-100 shadow-xl">
+                <figure>
+                    <img className="h-[200px]" src={product_image} alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <Link to={`/featureDetails/${_id}`}>
+                        <h2 className="card-title">Name: {product_name}</h2>
+                    </Link>
+                    <p className="space-x-3">
+                        <span className="font-bold text-blue-600">Tags:</span> <span>#{tags}</span>{" "}
+                    </p>
+                    <div className="card-actions justify-between items-center mt-4">
+                        <p>{timestamp}</p>
+                        {voteMail || mail || !user ? (
+                            <Link to="/login">
+                                <button className="btn text-blue-600 w-full mt-4 bg-gradient-to-r from-cyan-600 to-pink-300 " disabled>
+                                    <GiVote />
+                                    Upvote ({upvoted})
+                                </button>
+                            </Link>
+                        ) : (
+                            <button
+                                onClick={() => handleUpvote(_id)}
+                                className="btn text-white w-full mt-4 bg-gradient-to-r from-cyan-600 to-pink-300"
+                            >
+                                <GiVote />
+                                Upvote ({upvoted})
+                            </button>
+                        )}
                     </div>
                 </div>
-            
+            </div>
         </div>
     );
 };
--- src/Components/Pages/Home/FeatureProduct.jsx
+++ src/Components/Pages/Home/FeatureProduct.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
 import Feature from "./Feature";
 import { useQuery } from "@tanstack/react-query";
@@ -8,7 +8,7 @@ const FeatureProduct = () => {
 
     const axiosPublic = useAxiosPublic()
     
-    const [asc,setAsc]=useState(true)
+    
     const mark = 'featured'
 
     
@@ -16,7 +16,7 @@ const FeatureProduct = () => {
     const {refetch,  data: feature = [] } = useQuery({
         queryKey: ['feature'],
         queryFn: async () => {
-            const res = await axiosPublic.get(`/addProduct?sort=${asc ? 'asc' : 'des'}`)
+            const res = await axiosPublic.get('/addProduct')
             return res.data
 
         }
@@ -31,9 +31,7 @@ const FeatureProduct = () => {
     return (
         <div>
             <h2 className="text-center font-bold text-3xl italic mt-16 mb-8">Feature Products</h2>
-            <div className="text-center mb-8">
-                <button onClick={()=>setAsc(!asc)} className="btn btn-secondary">{ asc ? 'latest' : 'oldest'}</button>
-            </div>
+            
            <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 md:ml-48 ml-5 lg:ml-28">
             {
                 featured.slice(0,4).map(product=><Feature key={product._id} product={product} refetch={refetch}></Feature>)
